5b4f8ceca338fc470a8bc1f0221a61dc9519aa5a
jessicahaara <jessica.haara@iths.se>
2021-02-16 17:32:32 +0100

    Css fixes profile

--- airbean-project/src/components/CreateProfile.vue
+++ airbean-project/src/components/CreateProfile.vue
@@ -1,32 +1,37 @@
 <template>
-<div class="create">
-    <img src="../assets/logo.svg" alt="logo">
-    <h2 class="welcome">
-        Välkommen till AirBean-familjen!
-    </h2>
+  <div class="create">
+    <img src="../assets/logo.svg" alt="logo" />
+    <h2 class="welcome">Välkommen till AirBean-familjen!</h2>
     <p>
-        Genom att skapa ett konto nedan kan du spara och se din orderhistorik.
+      Genom att skapa ett konto nedan kan <br />
+      du spara och se din orderhistorik.
     </p>
-</div>
-    
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-
 .create {
-    background-color: #f3e4e1;
-    width: 100%;
-    height: 100%;
+  background-color: #f3e4e1;
+  width: 100%;
+  height: 100%;
+  text-align: center;
 }
 
 img {
-    margin-top: 6%;
+  margin-top: 6%;
 }
 
+h2 {
+  margin: 0;
+  font-size: 32px;
+}
+
+p {
+  margin: 2%;
+  font-size: 16px;
+}
 </style>
